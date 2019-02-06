0d13494f7316b33159c0fcc1e1c5860e1c98a613
Kevin Wu <kevinwu@Kevins-MacBook-Air-4.local>
2019-02-06 09:50:05 -0500

    Skeleton

---
+++ cs441_assignment2/cs441_assignment2/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  cs441_assignment2
+//
+//  Created by Kevin Wu on 2/6/19.
+//  Copyright © 2019 Kevin Wu. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
